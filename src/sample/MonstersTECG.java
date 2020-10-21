e754241c7027cdf352e6eb5f0bf7f0c06349c9a1
Kearranza <cbkevin.ant@gmail.com>
2020-10-20 22:19:02 -0600

    Adicción de botones y textfields

--- src/sample/MonstersTECG.java
+++ src/sample/MonstersTECG.java
@@ -4,18 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MonstersTECG extends Application {
 
     @Override
     public void start(Stage loginStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
         loginStage.setTitle("MonstersTECG");
         root.setId("pane");
         Scene scene = new Scene(root, 1280, 720);
-        scene.getStylesheets().addAll(this.getClass().getResource("loginFondo.css").toExternalForm());
+        //scene.getStylesheets().addAll(this.getClass().getResource("loginFondo.css").toExternalForm());
         loginStage.setScene(scene);
         loginStage.show();
     }
