2c41cfcf05315b8805ce99b717f7c7d8b7e2440f
Kearranza <cbkevin.ant@gmail.com>
2020-10-20 20:45:30 -0600

    Corrección de ScreeBuilder

--- src/sample/MonstersTECG.java
+++ src/sample/MonstersTECG.java
@@ -1,19 +1,18 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
-
+import javafx.stage.Stage;
 
 public class MonstersTECG extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+
     @Override
     public void start(Stage loginStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         loginStage.setTitle("MonstersTECG");
-        StackPane root = new StackPane();
         root.setId("pane");
         Scene scene = new Scene(root, 1280, 720);
         scene.getStylesheets().addAll(this.getClass().getResource("loginFondo.css").toExternalForm());
@@ -22,6 +21,7 @@ public class MonstersTECG extends Application {
     }
 
 
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
-
