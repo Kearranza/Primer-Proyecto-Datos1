dda2f5f6c3976ef060e3f4c1c0ac071aa4087178
Kearranza <cbkevin.ant@gmail.com>
2020-10-20 20:48:55 -0600

    Reset Proyecto

---
+++ src/sample/MonstersTECG.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MonstersTECG extends Application {
+
+    @Override
+    public void start(Stage loginStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        loginStage.setTitle("MonstersTECG");
+        root.setId("pane");
+        Scene scene = new Scene(root, 1280, 720);
+        scene.getStylesheets().addAll(this.getClass().getResource("loginFondo.css").toExternalForm());
+        loginStage.setScene(scene);
+        loginStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
