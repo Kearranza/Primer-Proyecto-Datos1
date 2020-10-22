02e43d62c2397dd5d321bd382a2a0b47e31e00f3
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-10-21 19:26:01 -0600

    Programacion básica del primer boton

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    public void clickUnirse(){
+        System.out.println("Hola");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
