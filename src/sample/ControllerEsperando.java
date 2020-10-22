97c1f3fb0dfd75f415f79e6e97cfec774527a8c2
Kearranza <cbkevin.ant@gmail.com>
2020-10-21 20:30:10 -0600

    Incorporación de ventana de espera

---
+++ src/sample/ControllerEsperando.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerEsperando implements Initializable {
+    @FXML
+    public void clickUnirse(){
+        System.out.println("Hola");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
