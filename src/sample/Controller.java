dceed98336b7de92c9e3fe897fa6602f1e88db67
Kearranza <cbkevin.ant@gmail.com>
2020-10-21 21:42:23 -0600

    Cambio de Escena

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,23 +1,31 @@
 package sample;
 
 import javafx.beans.property.ObjectProperty;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-
     @FXML
-    public void clickUnirse(){
+    public void cambiarEsperar(ActionEvent event) throws IOException {
+        Parent esperandoParent = FXMLLoader.load(getClass().getResource("Esperando.fxml"));
+        Scene esperandoScene = new Scene(esperandoParent);
 
-    }
-    @FXML
-    public void clickHost() {
-
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(esperandoScene);
+        window.show();
     }
 
     @Override
--- src/sample/ControllerEsperando.java
+++ src/sample/ControllerEsperando.java
@@ -1,15 +1,27 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerEsperando implements Initializable {
     @FXML
-    public void clickUnirse(){
-        System.out.println("Hola");
+    public void cambiarMenu(ActionEvent event) throws IOException {
+        Parent menuParent = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        Scene menuScene = new Scene(menuParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(menuScene);
+        window.show();
     }
 
     @Override
