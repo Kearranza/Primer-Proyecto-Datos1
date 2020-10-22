1a93ee23abced14cc5917ece1ebab60fec36f38f
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-10-21 22:09:14 -0600

    Excepciones en casillas de puerto e ip

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextField;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
@@ -19,15 +22,91 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
-    public void cambiarEsperar(ActionEvent event) throws IOException {
-        Parent esperandoParent = FXMLLoader.load(getClass().getResource("Esperando.fxml"));
-        Scene esperandoScene = new Scene(esperandoParent);
+    private TextField ipP;
+    @FXML
+    private TextField ipE;
+    @FXML
+    private TextField portP;
+    @FXML
+    private TextField portE;
+    @FXML
+    public void clickHost(ActionEvent event) throws IOException {
+        String ipPer = ipP.getText();
+        String portPer = portP.getText();
+        boolean flag = true;
+        try{
+            String[] componentsP = ipPer.split("\\.");
+            for (int i = 0; i < componentsP.length;i++){
+                Integer.parseInt(componentsP[i]);
+                flag =false;
+            }
+            if (flag){
+                Integer.parseInt(ipPer);
+            }
+            Integer.parseInt(portPer);
 
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(esperandoScene);
-        window.show();
+            Parent esperandoParent = FXMLLoader.load(getClass().getResource("Esperando.fxml"));
+            Scene esperandoScene = new Scene(esperandoParent);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(esperandoScene);
+            window.show();
+        }
+        catch(NumberFormatException e){
+            ipP.setText("");
+            ipE.setText("");
+            portP.setText("");
+            portE.setText("");
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            DialogPane dialogPane = error.getDialogPane();
+            error.setTitle("Alerta");
+            error.setHeaderText("Hubo un error a la hora de procesar la información digitada");
+            error.setContentText("Se escribió de forma incorrecta, ya sea, un ip o un puerto, por favor escriba nada más números.");
+            error.showAndWait();
+        }
+
     }
+    @FXML
+    public void clickUnirse(ActionEvent event) throws IOException {
+        String ipPer = ipP.getText();
+        String portPer = portP.getText();
+        String ipEx = ipE.getText();
+        String portEx = portE.getText();
+        boolean flag = true;
+        try{
+            String[] componentsP = ipPer.split("\\.");
+            for (int i = 0; i < componentsP.length;i++){
+                Integer.parseInt(componentsP[i]);
+                flag =false;
+            }
+            if (flag){
+                Integer.parseInt(ipPer);
+            }
+            Integer.parseInt(portPer);
+            String[] componentsE = ipEx.split("\\.");
+            for (int i = 0; i < componentsE.length;i++){
+                Integer.parseInt(componentsE[i]);
+                flag =false;
+            }
+            if (flag){
+                Integer.parseInt(ipEx);
+            }
+            Integer.parseInt(portEx);
+        }
+        catch(NumberFormatException e){
+            ipP.setText("");
+            ipE.setText("");
+            portP.setText("");
+            portE.setText("");
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            DialogPane dialogPane = error.getDialogPane();
+            error.setTitle("Alerta");
+            error.setHeaderText("Hubo un error a la hora de procesar la información digitada");
+            error.setContentText("Se escribió de forma incorrecta, ya sea, un ip o un puerto, por favor escriba nada más números.");
+            error.showAndWait();
+        }
 
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
