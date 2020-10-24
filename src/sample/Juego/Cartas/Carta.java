ef9c5f2bc3964cb5300d0ea669fbb2e495183482
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-10-24 16:32:52 -0600

    Cambio en la clase carta

--- src/sample/Juego/Cartas/Carta.java
+++ src/sample/Juego/Cartas/Carta.java
@@ -6,13 +6,12 @@ import javafx.scene.image.ImageView;
 public class Carta {
     private Carta next = null;
     private Carta prev = null;
-    private String ubicacion;
-    private int coste;
     private String imagen;
+    private int coste;
     private Image image = new Image(getClass().getResourceAsStream(imagen));
     private ImageView imageView = new ImageView(image);
 
-    public Carta(int coste, String imagen, String ubicacion) {
+    public Carta(int coste, String imagen) {
         this.coste = coste;
         this.imagen = imagen;
     }
