9128452a439dc298054abf3e27e0eca49557f94a
Kearranza <cbkevin.ant@gmail.com>
2020-10-25 18:33:09 -0600

    Adición de servidor y cliente

--- src/sample/Conexion/Cliente.java
+++ src/sample/Conexion/Cliente.java
@@ -1,4 +1,58 @@
 package sample.Conexion;
 
-public class Cliente {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Bryan Martínez
+ */
+public class Cliente implements Runnable{
+
+    /**
+     *
+     */
+    public int puerto;
+
+    /**
+     *
+     */
+    public String mensaje;
+
+    /**
+     *
+     * @param puerto
+     * @param mensaje
+     */
+    public Cliente(int puerto, String mensaje){
+        this.puerto = puerto;
+        this.mensaje= mensaje;
+
+    }
+    /**
+     *Creacion del nuevo cliente y envio del mensaje
+     */
+    @Override
+    public void run() {
+        String host = "127.0.0.1";
+        DataOutputStream out;
+        try{
+            Socket sc = new Socket(host, puerto);
+            out = new DataOutputStream(sc.getOutputStream());
+            out.writeUTF(mensaje);
+            String[] components = mensaje.split("\\|");
+            //Chat chat = new Chat(String.valueOf(puerto), components[1]);
+            sc.close();
+
+        }catch(ConnectException e){
+            JOptionPane.showMessageDialog(null, "No se pudo realizar la conexion con el puerto");
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/sample/Conexion/Servidor.java
+++ src/sample/Conexion/Servidor.java
@@ -1,4 +1,71 @@
 package sample.Conexion;
 
-public class Servidor {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.io.DataInputStream;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Bryan Martínez
+ */
+public class Servidor extends Observable implements Runnable{
+
+    /**
+     *
+     */
+    public int puerto = 5000;
+
+    /**
+     *
+     */
+    public ServerSocket servidor = null;
+
+    /**
+     *
+     */
+    public Servidor (){
+        boolean flag =true;
+        while (flag){
+            try{
+                this.servidor = new ServerSocket(puerto);
+                flag = false;
+            }catch(IOException ex){
+                puerto+=1;
+            }
+        }
+        this.puerto= puerto;
+    }
+    /**
+     *Inicia el servidor y esta atento a recibir mensajes para notificar y crear un nuevo chat o agregar el mensaje a uno existente
+     */
+    @Override
+    public void run() {
+        Socket sc = null;
+        DataInputStream in;
+        try{
+            while(true){
+                sc = servidor.accept();
+                in = new DataInputStream(sc.getInputStream());
+
+                String mensaje = in.readUTF();
+                String[] components = mensaje.split("\\|");
+                //Chat chat = new Chat(components[0], components[1]);
+
+                this.setChanged();
+                this.notifyObservers(components[0]);
+                this.clearChanged();
+                sc.close();
+
+            }
+        }catch (IOException ex){
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+    }
+
 }
