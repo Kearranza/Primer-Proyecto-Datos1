f2ed4a1916fb407e84b0030004ae4c4abe46795c
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-10-26 16:34:32 -0600

    Datos inecesarios

--- src/sample/Conexion/Servidor.java
+++ src/sample/Conexion/Servidor.java
@@ -8,26 +8,12 @@ import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Bryan Martínez
- */
 public class Servidor implements Runnable{
 
-    /**
-     *
-     */
     public int puerto;
     private boolean conectado;
-    /**
-     *
-     */
     public ServerSocket servidor = null;
 
-    /**
-     *
-     * @return
-     */
     public Servidor(int puerto){
         try{
             this.servidor = new ServerSocket(puerto);
@@ -42,9 +28,6 @@ public class Servidor implements Runnable{
         return conectado;
     }
 
-    /**
-     *Inicia el servidor y esta atento a recibir mensajes para notificar y crear un nuevo chat o agregar el mensaje a uno existente
-     */
     @Override
     public void run() {
         Socket sc;
