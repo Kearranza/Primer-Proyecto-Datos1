380a4c8d85df55dcffddd47a2699d61a78192646
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-10-31 13:02:57 -0600

    Defeinicion de textarea para historial

--- src/sample/Controllers/ControllerTablero.java
+++ src/sample/Controllers/ControllerTablero.java
@@ -3,19 +3,21 @@ package sample.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerTablero implements Initializable {
     @FXML
+    private TextArea historial;
+    @FXML
     public void click(ActionEvent evente){
-        System.out.println("HOla");
+        historial.appendText("Hola"+"\n");
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 }
