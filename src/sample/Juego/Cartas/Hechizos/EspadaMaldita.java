2403c96016cc8c7146c0b5ed220221929212ddab
Kearranza <cbkevin.ant@gmail.com>
2020-11-05 20:38:41 -0600

    Corrección de cartas

--- src/sample/Juego/Cartas/Hechizos/EspadaMaldita.java
+++ src/sample/Juego/Cartas/Hechizos/EspadaMaldita.java
@@ -4,7 +4,7 @@ import sample.Juego.Cartas.Carta;
 
 public class EspadaMaldita extends Carta {
     public EspadaMaldita(int coste, String imagen) {
-        super(coste, imagen, "H", null);
+        super(coste, imagen, "H", true);
     }
     public void accion(){
 
